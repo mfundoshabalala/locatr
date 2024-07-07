8cf7b5733ea32973ef6b021efda52f23a4238dc0
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2024-07-07 21:34:38 +0200

    move project to frontend folder

--- libs/frontend/maps/src/components/map-display/map-display.component.ts
+++ libs/frontend/maps/src/components/map-display/map-display.component.ts
@@ -1,20 +1,15 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MapSearchComponent } from '../map-search/map-search.component';
-import { MapButtonPanelComponent } from '../map-button-panel/map-button-panel.component';
-import { MapInfoPanelComponent } from '../map-info-panel/map-info-panel.component';
-import { MapRoutingPanelComponent } from '../map-routing-panel/map-routing-panel.component';
-import { MapSidebarComponent } from '../map-sidebar/map-sidebar.component';
-import {
-  FooterComponent,
-  HeaderComponent,
-  LogoComponent,
-  NavbarComponent,
-  UserProfileComponent,
-  DatePickerComponent,
-} from '@profolio/shared-ui';
-import { MapControllerComponent } from '../map-controller/map-controller.component';
-import { DriverSelectorComponent } from '../driver-selector/driver-selector.component';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+import { MapSearchComponent } from "../map-search/map-search.component";
+import { MapSidebarComponent } from "../map-sidebar/map-sidebar.component";
+import { MapInfoPanelComponent } from "../map-info-panel/map-info-panel.component";
+import { MapControllerComponent } from "../map-controller/map-controller.component";
+import { DriverSelectorComponent } from "../driver-selector/driver-selector.component";
+import { MapButtonPanelComponent } from "../map-button-panel/map-button-panel.component";
+import { MapRoutingPanelComponent } from "../map-routing-panel/map-routing-panel.component";
+import { DatePickerComponent, FooterComponent, HeaderComponent, LogoComponent, NavbarComponent, UserProfileComponent } from "@profolio/frontend/shared/ui";
+
 
 @Component({
   selector: 'lib-map-display',
