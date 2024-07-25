48bab109da0d5184d85ebd721da4d5e34e61b0cf
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2024-07-25 21:06:44 +0200

    Update user.entity.ts

--- apps/locatr-backend/src/modules/user/entities/user.entity.ts
+++ apps/locatr-backend/src/modules/user/entities/user.entity.ts
@@ -6,31 +6,34 @@ import { Employee } from "../../employee/entities/employee.entity";
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid', { name: 'userID' })
-  id!: string;
+  id: string;
 
-  @Column({ unique: true })
-  username!: string;
+  @Column({ unique: true, type: 'varchar', length: 255 })
+  username: string;
 
-  @Column()
-  password!: string;
+  @Column({ unique: true, type: 'varchar', length: 255 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
 
   @OneToOne(() => Role, (role) => role.user)
-  @JoinColumn()
-  role!: Role; // assigned based on department and position
+  @JoinColumn({ name: 'roleID' })
+  role: Role; // assigned based on department and position
 
   @OneToOne(() => Employee, (employee) => employee.user, { cascade: true })
-  @JoinColumn()
-  employee!: Employee;
+  @JoinColumn({ name: 'employeeID' })
+  employee: Employee;
 
-  @CreateDateColumn({ type: 'timestamp', name: 'createdAt' })
-  createdAt!: Date;
+  @CreateDateColumn({ type: 'timestamp', update: false, default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
 
-  @Column({ nullable: true })
-  createdBy!: string;
+  @Column({ nullable: false, update: false, length: 255, type: 'varchar', default: 'system' })
+  createdBy: string;
 
-  @UpdateDateColumn({ type: 'timestamp', name: 'updatedAt' })
-  updatedAt!: Date;
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
 
-  @Column({ nullable: true })
-  updatedBy!: string;
+  @Column({ nullable: true, length: 255, type: 'varchar', default: 'system' })
+  updatedBy: string;
 }
