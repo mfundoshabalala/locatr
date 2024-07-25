7de746b4f610f63f024e3922f4ef8844f03d3aa3
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2024-07-25 21:00:54 +0200

    create contact resources

---
+++ apps/locatr-backend/src/modules/contact/dto/create-contact.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, IsNotEmpty, IsEmail } from "class-validator";
+
+export class CreateContactDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+	@IsString()
+	phone: string;
+
+	@IsEmail()
+	@IsNotEmpty()
+	email: string;
+
+	@IsString()
+	position: string;
+}
