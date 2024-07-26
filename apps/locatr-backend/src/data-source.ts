fcc64e73d82c422166fc66177240c80d8d32978b
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2024-07-26 04:54:58 +0200

    Update data-source.ts - put a default port if env variable is undefined

--- apps/locatr-backend/src/data-source.ts
+++ apps/locatr-backend/src/data-source.ts
@@ -6,7 +6,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: +process.env.POSTGRES_PORT,
+  port: +(process.env.POSTGRES_PORT || 5432),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
