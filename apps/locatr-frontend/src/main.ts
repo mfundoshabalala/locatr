fd56aaf2c4e549ed2f79eccd616e1c638adf1d5b
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2024-08-08 21:53:40 +0200

    configure vercel

--- apps/locatr-frontend/src/main.ts
+++ apps/locatr-frontend/src/main.ts
@@ -1,7 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 inject()
+injectSpeedInsights();
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
