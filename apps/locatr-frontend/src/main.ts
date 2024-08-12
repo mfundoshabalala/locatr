db3a4da5d4d08b3312840d94a0aed24afa9b2677
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2024-08-12 04:51:25 +0200

    update the domain regex

--- apps/locatr-frontend/src/main.ts
+++ apps/locatr-frontend/src/main.ts
@@ -8,15 +8,12 @@ import { AppComponent } from './app/app.component';
 
 
 Sentry.init({
-  dsn: "https://3b9f586426417b299fbf679039671e66@o4504668388065280.ingest.us.sentry.io/4507761951440896",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-    Sentry.replayIntegration(),
-  ],
+  dsn: 'https://3b9f586426417b299fbf679039671e66@o4504668388065280.ingest.us.sentry.io/4507761951440896',
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  tracePropagationTargets: ['localhost', /^https:\/\/locatr\.vercel\.app\/api/],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
