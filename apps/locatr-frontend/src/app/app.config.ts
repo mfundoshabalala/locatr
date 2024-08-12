17e48e23e053e88c49fba3cff2921bc8a2098abf
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2024-08-12 04:38:05 +0200

    add sentry for angular

--- apps/locatr-frontend/src/app/app.config.ts
+++ apps/locatr-frontend/src/app/app.config.ts
@@ -1,11 +1,30 @@
+import { APP_INITIALIZER, ErrorHandler, NgModule } from "@angular/core";
+import * as Sentry from "@sentry/angular";
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    {
+      provide: ErrorHandler,
+      useValue: Sentry.createErrorHandler({
+        showDialog: true,
+      }),
+    }, {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      useFactory: () => () => {},
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideAnimations(),
--- apps/locatr-frontend/src/main.ts
+++ apps/locatr-frontend/src/main.ts
@@ -1,9 +1,26 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { inject } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import * as Sentry from "@sentry/angular";
 
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+
+Sentry.init({
+  dsn: "https://3b9f586426417b299fbf679039671e66@o4504668388065280.ingest.us.sentry.io/4507761951440896",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 inject()
 injectSpeedInsights();
