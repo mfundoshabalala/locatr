7159649caaaf9be0ba63d626e9e70c132d61ba12
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2024-08-12 07:07:59 +0200

    UPDATE

--- apps/locatr-backend/src/main.ts
+++ apps/locatr-backend/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.LOCATR_PORT || 3000;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   // Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
   Logger.log(`🚀 Application is running on: ${await app.getUrl()}`);
