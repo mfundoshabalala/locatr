a805609ca5b56785655f822b8d9b730e51f70b11
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2024-09-08 00:19:37 +0200

    configure swagger

--- apps/locatr-backend/src/main.ts
+++ apps/locatr-backend/src/main.ts
@@ -7,6 +7,8 @@ import "./instrument.ts";
 
 import * as dotenv from 'dotenv';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app/app.module';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
@@ -15,6 +17,15 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('Locatr')
+  .setDescription('Locatr API Documentation')
+  .setVersion('1.0.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const isProduction = process.env.NODE_ENV === 'production';
 
   app.enableCors({
@@ -24,9 +35,10 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  const host = process.env.HOST || '0.0.0.0';
+  await app.listen(port, host);
   // Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
-  Logger.log(`🚀 Application is running on: ${ isProduction ? process.env.LOCATR_API_URL : 'http://localhost:3000/api' }`);
+  Logger.log(`🚀 Application is running on: ${ isProduction ? port + '/' + globalPrefix : 'http://localhost:3000/api' }`);
 }
 
 bootstrap();
